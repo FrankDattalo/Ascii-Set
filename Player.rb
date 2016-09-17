753d4eaeb5eca70e69be603b73834792f3be9d79
mayow <mayow.1@osu.edu>
2016-09-17 12:37:01 -0400

    tab and spaces change

--- Player.rb
+++ Player.rb
@@ -1,15 +1,15 @@
 class Player
 
 
-	attr_accessor :score
-	attr_reader :name
+  attr_accessor :score
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @score = 0
   end
 
-	def to_json(options = nil)
+  def to_json(options = nil)
 		'{"name":%s,"score":%s}' % [@name.to_json, @score.to_json]
   end
 end
