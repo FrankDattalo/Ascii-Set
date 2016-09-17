361234f2b75e7914870ea15ad72b65751f47ab39
mayow <mayow.1@osu.edu>
2016-09-17 12:47:02 -0400

    tab and spaces change

--- API.rb
+++ API.rb
@@ -6,30 +6,28 @@ require_relative './Card'
 require_relative './Server'
 
 get '/deck' do
-	json Server.instance.data
+  json Server.instance.data
 end
 
 get '/auth' do
-	name = params['name']
+  name = params['name']
 
   accepted = false
-	if !name.nil? && name != "" && !Server.instance.contains_player_name?(name) &&
-		!Server.instance.started
+  if !name.nil? && name != "" && !Server.instance.contains_player_name?(name) &&
+      !Server.instance.started
 
-		Server.instance.add_player Player.new name
-		accepted = true
-	end
-
-	json accepted: accepted
+    Server.instance.add_player Player.new name
+    accepted = true
+  end
+  json accepted: accepted
 end
 
 get '/un-register' do
-	name = params['name']
-
-	if !name.nil? && name != "" && Server.instance.contains_player_name?(name)
-		Server.instance.remove_player name
-	end
-	json response: 'ok'
+  name = params['name']
+  if !name.nil? && name != "" && Server.instance.contains_player_name?(name)
+    Server.instance.remove_player name
+  end
+  json response: 'ok'
 end
 
 get '/players' do
@@ -37,9 +35,9 @@ get '/players' do
 end
 
 get '/start' do
-	hasStarted = Server.instance.started
-	Server.instance.start if !hasStarted
-	json game_playable: !hasStarted
+  hasStarted = Server.instance.started
+  Server.instance.start if !hasStarted
+  json game_playable: !hasStarted
 end
 
 get '/new' do
@@ -48,17 +46,16 @@ get '/new' do
 end
 
 get '/check-set' do
-	name = params['name']
-	card1 = params['card1'].capitalize
-	card2 = params['card2'].capitalize
-	card3 = params['card3'].capitalize
-
-	player = Server.instance.get_player_by_name name
-	active_cards = Server.instance.active_cards
+  name = params['name']
+  card1 = params['card1'].capitalize
+  card2 = params['card2'].capitalize
+  card3 = params['card3'].capitalize
+  player = Server.instance.get_player_by_name name
+  active_cards = Server.instance.active_cards
 
 	response = false
 
-	if card1 != card2 && card2 != card3 && card1 != card3 &&
+  if card1 != card2 && card2 != card3 && card1 != card3 &&
 		Server.instance.started && active_cards.key?(card1) &&
 		active_cards.key?(card2) &&
 		active_cards.key?(card3) &&
@@ -68,23 +65,22 @@ get '/check-set' do
 
 		player.score += 50 # a set
 
-		Server.instance.remove_card_from_play active_cards[card1]
-		Server.instance.remove_card_from_play active_cards[card2]
-		Server.instance.remove_card_from_play active_cards[card3]
+    Server.instance.remove_card_from_play active_cards[card1]
+    Server.instance.remove_card_from_play active_cards[card2]
+    Server.instance.remove_card_from_play active_cards[card3]
 
-		Server.instance.deal_up_to_12
-
-		Server.instance.re_index_cards
-		Server.instance.update_game_over
+	  Server.instance.deal_up_to_12
+    Server.instance.re_index_cards
+	  Server.instance.update_game_over
 		response = true
 
-		Server.instance.reset_stuck_count
+	  Server.instance.reset_stuck_count
 
-	elsif Server.instance.started
-		player.score -= 5 # not a set
+  elsif Server.instance.started
+  player.score -= 5 # not a set
 	end
 
-	json is_set: response
+  json is_set: response
 end
 
 
@@ -93,13 +89,12 @@ get '/stuck' do
 	Server.instance.increase_stuck_players name
 
 	if Server.instance.all_players_stuck?
-		if Server.instance.set_in_play? then
-			Server.instance.deal_hints
-		else
-			Server.instance.deal_3_more_cards
-			Server.instance.reset_stuck_count
-		end
-	end
-
-	json stuck_count: Server.instance.stuck_player_count
+   if Server.instance.set_in_play? then
+  Server.instance.deal_hints
+  else
+  Server.instance.deal_3_more_cards
+  Server.instance.reset_stuck_count
+   end
+   end
+  json stuck_count: Server.instance.stuck_player_count
 end
